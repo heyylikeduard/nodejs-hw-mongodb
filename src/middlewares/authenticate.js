a164c6de288c852caa95cb175b53391c209142bc
Eduard Heyylik <edosheyylik@gmail.com>
2025-03-22 13:06:45 +0200

    fix1

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -7,7 +7,7 @@ export const authenticate = async (req, res, next) => {
   // Отримання токена з заголовка Authorization
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw createHttpError(401, 'Access token is missing or invalid');
+    return next(createHttpError(401, 'Access token is missing or invalid'));
   }
 
   const accessToken = authHeader.split(' ')[1]; // Вилучаємо токен з рядка "Bearer <token>"
@@ -18,26 +18,26 @@ export const authenticate = async (req, res, next) => {
     decoded = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET);
   } catch (error) {
     if (error.name === 'TokenExpiredError') {
-      throw createHttpError(401, 'Access token expired');
+      return next(createHttpError(401, 'Access token expired'));
     }
-    throw createHttpError(401, 'Invalid access token');
+    return next(createHttpError(401, 'Invalid access token'));
   }
 
   // Пошук сесії за access токеном
   const session = await Session.findOne({ accessToken });
   if (!session) {
-    throw createHttpError(401, 'Session not found');
+    return next(createHttpError(401, 'Session not found'));
   }
 
   // Перевірка, чи не закічився термін дії access токена
   if (session.accessTokenValidUntil < new Date()) {
-    throw createHttpError(401, 'Access token expired');
+    return next(createHttpError(401, 'Access token expired'));
   }
 
   // Пошук користувача за ID
   const user = await User.findById(decoded.userId);
   if (!user) {
-    throw createHttpError(401, 'User not found');
+    return next(createHttpError(401, 'User not found'));
   }
 
   // Додаємо користувача до об'єкту запиту
